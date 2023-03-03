2aa3b94a33e87f89f088db8d0000f121c60c8c63
Павел Золотарёв <121239662+pashtet018@users.noreply.github.com>
2023-03-03 17:44:30 +0300

    Delete src directory

--- src/components/Container/Container.jsx
+++
@@ -1,8 +0,0 @@
-import style from './Container.module.css';
-import classNames from 'classnames';
-
-export const Container = ({ children, className }) => (
-    <div className={classNames(style.container, className)}>
-        {children}
-    </div>
-);
--- src/const.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = ' https://sticky-ordinary-salmon.glitch.me';
-export const POSTFIX = '/api/product';
